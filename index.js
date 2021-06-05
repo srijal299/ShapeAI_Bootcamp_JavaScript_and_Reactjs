cfa9a5535535c5acc24b18704fac1e53ad268dd8
srijal299 <78316770+srijal299@users.noreply.github.com>
2021-06-05 14:07:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import Header from "./components/Header";
+// import List from "./components/List";
+import App from "./components/App";
+
+
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
